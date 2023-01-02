724bca060a1383f8435761479506569e1eff2801
nadia <nadiaelhadri99@gmail.com>
2023-01-02 10:24:47 +0100

    mutation : add and delete project & update client & project

--- .history/server/schema/schema_20230102100421.js
+++ .history/server/schema/schema_20230102100421.js
@@ -143,47 +143,47 @@ const mutation = new GraphQLObjectType({
       },
     },
     // Delete a project
-    deleteProject: {
-      type: ProjectType,
-      args: {
-        id: { type: GraphQLNonNull(GraphQLID) },
-      },
-      resolve(parent, args) {
-        return Project.findByIdAndRemove(args.id);
-      },
-    },
+    // deleteProject: {
+    //   type: ProjectType,
+    //   args: {
+    //     id: { type: GraphQLNonNull(GraphQLID) },
+    //   },
+    //   resolve(parent, args) {
+    //     return Project.findByIdAndRemove(args.id);
+    //   },
+    // },
     // Update a project
-    updateProject: {
-      type: ProjectType,
-      args: {
-        id: { type: GraphQLNonNull(GraphQLID) },
-        name: { type: GraphQLString },
-        description: { type: GraphQLString },
-        status: {
-          type: new GraphQLEnumType({
-            name: 'ProjectStatusUpdate',
-            values: {
-              new: { value: 'Not Started' },
-              progress: { value: 'In Progress' },
-              completed: { value: 'Completed' },
-            },
-          }),
-        },
-      },
-      resolve(parent, args) {
-        return Project.findByIdAndUpdate(
-          args.id,
-          {
-            $set: {
-              name: args.name,
-              description: args.description,
-              status: args.status,
-            },
-          },
-          { new: true }
-        );
-      },
-    },
+    // updateProject: {
+    //   type: ProjectType,
+    //   args: {
+    //     id: { type: GraphQLNonNull(GraphQLID) },
+    //     name: { type: GraphQLString },
+    //     description: { type: GraphQLString },
+    //     status: {
+    //       type: new GraphQLEnumType({
+    //         name: 'ProjectStatusUpdate',
+    //         values: {
+    //           new: { value: 'Not Started' },
+    //           progress: { value: 'In Progress' },
+    //           completed: { value: 'Completed' },
+    //         },
+    //       }),
+    //     },
+    //   },
+    //   resolve(parent, args) {
+    //     return Project.findByIdAndUpdate(
+    //       args.id,
+    //       {
+    //         $set: {
+    //           name: args.name,
+    //           description: args.description,
+    //           status: args.status,
+    //         },
+    //       },
+    //       { new: true }
+    //     );
+    //   },
+    // },
   },
 });
 module.exports = new GraphQLSchema({
